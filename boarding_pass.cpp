e40b83c3ac1eff135f82b700ede2257bd0e12517
Sniphyric <sniphyric@gmail.com>
2026-03-17 22:30:39 -0300

    Add: Testing Text

---
+++ boarding_pass.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "++++++++++++++++++++++++++++++++++++++" << std::endl;
+    std::cout << "+ PASSENGER: Sniphyric               +" << std::endl;
+    std::cout << "+ DESTINY: Waterloo, Canada          +" << std::endl;
+    std::cout << "+ YEAR: 2027                         +" << std::endl;
+    std::cout << "+ ---------------------------------- +" << std::endl;
+    std::cout << "+ KEEP FOCUSING ON THE CODE!         +" << std::endl;
+    std::cout << "++++++++++++++++++++++++++++++++++++++" << std::endl;
+    return 0;
+}
