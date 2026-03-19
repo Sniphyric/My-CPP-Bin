79726682cd29129ce8cbce3455e411708fd57731
Sniphyric <sniphyric@gmail.com>
2026-03-19 19:00:30 -0300

    Testing Numbers

--- input_numbers.cpp
+++ input_numbers.cpp
@@ -8,5 +8,9 @@ int main()
     int z{};
     std::cin >> x >> y >> z;
     std::cout << "You've entered: " << x << ", " << y << ", and " << z << '\n';
+    
+    std::cin.ignore(1000, '\n');
+    std::cout << "Press enter to exit...";
+    std::cin.get();
     return 0;
 }
