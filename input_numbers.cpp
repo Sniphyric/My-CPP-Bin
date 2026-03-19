90b43b0b2beaa24680aa816b0137a81fa174d501
Sniphyric <sniphyric@gmail.com>
2026-03-19 18:05:26 -0300

    Testing Numbers

---
+++ input_numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter three different numbers each followed by a space: ";
+    int x{};
+    int y{};
+    int z{};
+    std::cin >> x >> y >> z;
+    std::cout << "You've entered: " << x << ", " << y << ", and " << z << '\n';
+    return 0;
+}
