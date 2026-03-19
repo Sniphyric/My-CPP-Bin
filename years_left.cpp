226011f05eda6f442c3394306392418eef6ca157
Sniphyric <sniphyric@gmail.com>
2026-03-19 18:49:49 -0300

    2030 Calculator

--- years_left.cpp
+++ years_left.cpp
@@ -7,9 +7,11 @@ int main()
     int currentYear{};
     std::cin >> currentYear; // this is going to extract what the user will write.
     
-    int yearsLeft{ 2030 - currentYear }; // this is going to subtract "2030" with what the user wrote before.
+    int yearsLeft{ 2030 - currentYear };
     std::cout << "There is " << yearsLeft << " years left for 2030." << '\n';
     
+    std::cin.ignore(1000, '\n');
+    std::cout << "Press enter to exit...";
     std::cin.get();
     return 0;
 }
